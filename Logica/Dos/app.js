c845425d1d5b8fc81f4e565e9a31a3606d024611
INRIAS <inrialva@gmail.com>
2024-06-27 23:35:05 -0500

    Example Two Js

---
+++ Logica/Dos/app.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector("h1");
+
+titulo.innerHTML="Juego del Número Secreto"
+
+let parrafo=document.querySelector("p");
+
+parrafo.innerHTML="Indica un número del 1 al 10"
+let btn=alert("Probando...!!!")
+
+let boton=document.querySelector("button");
+boton=document.addEventListener(onclick, btn());
